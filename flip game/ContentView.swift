cea811ff204f440cbca9171ca897a643333abb11
Kaptan <kaptanbamaniya@gmail.com>
2026-02-16 01:20:16 -0800

    Initial Commit

---
+++ flip game/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  flip game
+//
+//  Created by applelab03 on 2/16/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ flip game/flip_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  flip_gameApp.swift
+//  flip game
+//
+//  Created by applelab03 on 2/16/26.
+//
+
+import SwiftUI
+
+@main
+struct flip_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
